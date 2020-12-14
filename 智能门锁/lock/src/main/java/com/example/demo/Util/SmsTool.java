f43dff5396a295c4860d158e19da80732353ea67
mengxiangying <1984997880@qq.com>
2020-12-14 14:51:48 +0800

    注释掉了AK

--- 智能门锁/lock/src/main/java/com/example/demo/Util/SmsTool.java
+++ 智能门锁/lock/src/main/java/com/example/demo/Util/SmsTool.java
@@ -22,8 +22,8 @@ public class SmsTool {
     //产品域名,开发者无需替换
     static final String domain = "dysmsapi.aliyuncs.com";
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAI4GEMC5x5wM3NfqpN7NPt";
-    static final String accessKeySecret = "mKgT7EEYNbcY0RjFHn5lg8h2TJ5TWn";
+//    static final String accessKeyId = "LTAI4GEMC5x5wM3NfqpN7NPt";
+//    static final String accessKeySecret = "mKgT7EEYNbcY0RjFHn5lg8h2TJ5TWn";
 
     public static SendSmsResponse sendSms(String phone , String code, String TemplateCode) throws ClientException {
         //可自助调整超时时间
@@ -41,9 +41,9 @@ public class SmsTool {
         request.setPhoneNumbers(phone);
 
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("签名");
+        request.setSignName("智纹");
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("SMS_152440521");
+        request.setTemplateCode("SMS_206546093");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         request.setTemplateParam(code);
 
